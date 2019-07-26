7fe98006237fd1619a18d6dcefec400c64ea58fa
Tan Dinh <tan.dinh@isobar.com>
2019-07-26 14:08:42 +0700

    first product

---
+++ DGTeam/Reindex/Controller/Adminhtml/Indexer/MassImmediately.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace DGTeam\Reindex\Controller\Adminhtml\Indexer;
+
+class MassImmediately extends \Magento\Indexer\Controller\Adminhtml\Indexer
+{
+
+    /**
+     * @var Magento\Framework\Indexer\IndexerInterface $indexer
+     */
+    private $indexer;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Indexer\IndexerInterface $indexer
+
+    )
+    {
+        $this->indexer = $indexer;
+        parent::__construct($context);
+    }
+
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     */
+    public function execute()
+    {
+
+        $indexerIds = $this->getRequest()->getParam('indexer_ids');
+        if (!is_array($indexerIds)) {
+            $this->messageManager->addError(__('Please select indexers.'));
+        } else {
+            try {
+                foreach ($indexerIds as $indexerId) {
+
+                    /** @var Magento\Framework\Indexer\IndexerInterface $model */
+                    $this->indexer->load($indexerId)->reindexAll();
+
+                }
+
+                $this->messageManager->addSuccess(
+                    __('%1 indexer(s) are in "reindex required" mode and waiting for reindex by cronjob.', count($indexerIds))
+                );
+
+            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addException(
+                    $e,
+                    __("We couldn't change indexer(s)' mode because of the error: ". $e->getMessage())
+                );
+            }
+        }
+        $this->_redirect('*/*/list');
+
+    }
+
+    /**
+     * Check the controller alc
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Magento_Indexer::changeMode');
+    }
+}
---
+++ DGTeam/Reindex/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'DGTeam_Reindex',
+    __DIR__
+);
